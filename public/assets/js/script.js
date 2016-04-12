cbd3386303e34dd8a3bfe4f99f23fd5ac565f333
RgBerou <brotark@gmail.com>
2016-04-11 17:18:00 -0800

    Initial chat UI semi working

---
+++ public/assets/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+	$('.chat_header').click(function(){
+		$('.chat_body').slideToggle('slow');
+	});
+
+	$('.msg_header').click(function(){
+		$('.msg_wrap').slideToggle('slow');
+	});
+
+	$('.close').click(function(){
+		$('.msg_box').hide();
+	});
+
+	$('.users').click(function(){
+		$('.msg_wrap').show();
+		$('.msg_box').show();
+	});
+
+	$('textarea').keypress(
+    function(e){
+        if (e.keyCode == 13) {
+            var msg = $(this).val();
+			$(this).val('');
+			if(msg!='')
+			$('<div class="msg_b">'+msg+'</div>').insertBefore('.msg_insert');
+			$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
+        }
+    });
+
+
+});
